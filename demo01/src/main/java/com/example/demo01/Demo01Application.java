a8cb9db62902d90e37a4ffb801561726bf4c1dd3
qiang54 <qiangzhaoo@163.com>
2018-08-21 15:53:23 +0800

    filter

--- demo01/src/main/java/com/example/demo01/Demo01Application.java
+++ demo01/src/main/java/com/example/demo01/Demo01Application.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+
+/**
+ * 注意：@ServletComponentScan 在程序入口main类上注解！！！！
+ */
 @SpringBootApplication
 @ServletComponentScan
 public class Demo01Application {
